db539d0df10e7f926a3090704524847188c6c34a
gergefis <votanonkipos@gmail.com>
2024-08-08 01:08:12 +0300

    appropriate URI

--- src/main/java/com/mysite/demo/app/recipeat/controller/RecipeRestController.java
+++ src/main/java/com/mysite/demo/app/recipeat/controller/RecipeRestController.java
@@ -3,26 +3,19 @@ package com.mysite.demo.app.recipeat.controller;
 
 import com.mysite.demo.app.recipeat.service.RecipeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
+@RequiredArgsConstructor  //Lombok: δηλώνει τον Constructor kai ta final field
 public class RecipeRestController {
 
 
 	private final RecipeService recipeService;
 
-	//	inject Recipe Service (use constructor injection)
-//	@Autowired
-//	public RecipeRestController(RecipeService theRecipeService, RestTemplate theRestTemplate) {
-//		recipeService = theRecipeService;
-//	}
 
 
 	@GetMapping("/recipes")
--- src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
+++ src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
@@ -2,8 +2,6 @@ package com.mysite.demo.app.recipeat.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,8 +15,8 @@ public class RecipeService {
 
 //	@Value("{recipe.search}")
 
-		private   String XRapidApiKey = "5dfsfdf7as8gd5gd6f2as07";
-		private final String api_url = "www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+//		private   String XRapidApiKey = "5dfsfdf7as8gd5gd6f2as07";
+		private final String api_url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
 //		private final RecipeRepository repository;
 
 	@Autowired
@@ -36,7 +34,8 @@ public class RecipeService {
 //				headers.set("null", "null");
 
 //				Call to the API endpoint
-				ResponseEntity<String> response = restTemplate.exchange(api_url, HttpMethod.GET, null,/*new HttpEntity<>(headers),*/ String.class);
+//				ResponseEntity<String> response = restTemplate.exchange(api_url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+				ResponseEntity<String> response = restTemplate.getForEntity(api_url, String.class);
 
 				log.info("Output for a API: {}", response.getBody());
 
