eb8eeb59c489a09a28928b0eb2459e1c503c39d1
gergefis <votanonkipos@gmail.com>
2024-08-08 22:56:51 +0300

    Add Lombok - Decrease boilerplate code

--- src/main/java/com/mysite/demo/app/recipeat/entity/MealResponse.java
+++ src/main/java/com/mysite/demo/app/recipeat/entity/MealResponse.java
@@ -1,5 +1,62 @@
 package com.mysite.demo.app.recipeat.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+//BEGIN - Update entity class to Lombok to decrease boilerplate
+//...
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealResponse {
+
+	private List<Meal> meals;
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Meal {
+		private String idMeal;
+		private String strMeal;
+		private String strDrinkAlternate;
+		private String strCategory;
+		private String strArea;
+		private String strInstructions;
+		private String strMealThumb;
+		private String strYoutube;
+		private String strIngredient1;
+		private String strIngredient2;
+		private String strIngredient3;
+		private String strIngredient4;
+		private String strIngredient5;
+		private String strIngredient6;
+		private String strIngredient7;
+		private String strIngredient8;
+		private String strMeasure1;
+		private String strMeasure2;
+		private String strMeasure3;
+		private String strMeasure4;
+		private String strMeasure5;
+		private String strMeasure6;
+		private String strMeasure7;
+		private String strMeasure8;
+		// Όλα τα άλλα πεδία μέχρι strIngredient20 και strMeasure20
+	}
+}
+//	END - Update entity class to Lombok to decrease boilerplate
+
+
+
+
+
+/*
+package com.mysite.demo.app.recipeat.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -278,3 +335,4 @@ public class MealResponse {
 
 	}
 }
+*/
