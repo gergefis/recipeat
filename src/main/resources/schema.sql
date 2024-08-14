d52e5560e77947c20ffdccee271d7990871869a6
gergefis <votanonkipos@gmail.com>
2024-08-14 14:42:03 +0300

    Create table meal with sql query

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,28 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-create table Meal(
-    id int not null,
-    user_id int not null ,
-    title varchar(100),
-    completed boolean,
-    version int
-);
+create table meal(
+    idMeal varchar(100),
+    strMeal varchar(100),
+    strDrinkAlternate varchar(100),
+    strCategory varchar(100),
+    strArea varchar(100),
+    strInstructions varchar(255),
+    strMealThumb varchar(100),
+    strYoutube varchar(100),
+    strIngredient1 varchar(100),
+    strIngredient2 varchar(100),
+    strIngredient3 varchar(100),
+    strIngredient4 varchar(100),
+    strIngredient5 varchar(100),
+    strIngredient6 varchar(100),
+    strIngredient7 varchar(100),
+    strIngredient8 varchar(100),
+    strMeasure1 varchar(100),
+    strMeasure2 varchar(100),
+    strMeasure3 varchar(100),
+    strMeasure4 varchar(100),
+    strMeasure5 varchar(100),
+    strMeasure6 varchar(100),
+    strMeasure7 varchar(100),
+    strMeasure8 varchar(100)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
