6d0cab0c93f211226376c1bd19946730a563e63a
gergefis <votanonkipos@gmail.com>
2024-08-23 13:15:40 +0300

    fetch recipes from API

--- src/main/java/com/mysite/demo/app/recipeat/controller/HomeController.java
+++ src/main/java/com/mysite/demo/app/recipeat/controller/HomeController.java
@@ -1,17 +1,25 @@
 package com.mysite.demo.app.recipeat.controller;
 
 import com.mysite.demo.app.recipeat.rest.CategoryResponse;
+import com.mysite.demo.app.recipeat.rest.MealResponse;
+import com.mysite.demo.app.recipeat.rest.RecipeNameByCategoryResponse;
 import com.mysite.demo.app.recipeat.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor  //Lombok: δηλώνει τον Constructor kai ta final field
+@Slf4j
 public class HomeController {
 
 	@Autowired
@@ -19,31 +27,63 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String index(Model model) {
-		CategoryResponse categoryResponse = recipeService.getRecipesByCategory();
+		CategoryResponse categoryResponse = recipeService.getCategory();
 
 		// Προσθέτουμε τις κατηγορίες στο μοντέλο για να τις χρησιμοποιήσουμε στο template
 		model.addAttribute("categories", categoryResponse.getCategories());
+
 		// Επιστρέφει το όνομα του template χωρίς την επέκταση .html
 		return "index";
-	}
-
-	@GetMapping("/recipes")
-	public String sayHello(Model theModel) {
-		LocalDateTime currentDate = java.time.LocalDateTime.now();
-		theModel.addAttribute("theDate", currentDate);
-
-		return "recipes";
 	}
 
 	@GetMapping("/categories")
 	public String getCategories(Model model) {
 		// Παίρνουμε τις κατηγορίες μέσω του service
-		CategoryResponse categoryResponse = recipeService.getRecipesByCategory();
+		CategoryResponse categoryResponse = recipeService.getCategory();
 
 		// Προσθέτουμε τις κατηγορίες στο μοντέλο για να τις δούμε στη σελίδα
 		model.addAttribute("categories", categoryResponse.getCategories());
 
 		// Επιστρέφουμε το όνομα του template χωρίς την επέκταση .html
 		return "categories";
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "index";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@RequestParam("mealName") String mealName, Model model) {
+		MealResponse mealResponse = recipeService.getRecipesByName(mealName);
+
+		try {
+			LocalDateTime currentDate = java.time.LocalDateTime.now();
+			model.addAttribute("theDate", currentDate);
+
+			// Προσθέτουμε τη συνταγή στο μοντέλο για να τις χρησιμοποιήσουμε στο template
+				model.addAttribute("theMeal", mealResponse.getMeals());
+
+			return "meal";
+		} catch (Exception e) {
+			log.error(" - Something goes wrong while RETURN Json Object of recipes -",e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Exception while RETURN json Object to Model \"meal.html\"", e);
+		}
+	}
+	@RequestMapping("/fetchMealNameByCategory")
+	public String fetchMealNameFromCategory(@RequestParam("strMeal") String strMeal, Model model) {
+		MealResponse strMealResponse = recipeService.getRecipesByCategory(strMeal);
+
+		try {
+			// Προσθέτουμε τη συνταγή στο μοντέλο για να τις χρησιμοποιήσουμε στο template
+			model.addAttribute("strMeal", strMealResponse);
+
+			return "categories";
+		}catch (Exception e) {
+			log.error(" - Something goes wrong while getting value from Category Drop-Down menu -",e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Exception while RETURN json Object to Model \"index.html\"", e);
+		}
 	}
 }
--- src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
+++ src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
@@ -1,6 +1,7 @@
 package com.mysite.demo.app.recipeat.service;
 
 import com.mysite.demo.app.recipeat.rest.CategoryResponse;
+import com.mysite.demo.app.recipeat.rest.MealResponse;
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
-
 // 		I will create the endpoint
 @Service
 @Slf4j
 public class RecipeService {
-	//**************** Get the meal from client
-//		** Τα Headers
-//		private   String XRapidApiKey = "5dfsfdf7as8gd5gd6f2as07";
-	// Βασικό URI του API
-//		private final RecipeRepository repository;
+	//**************** Get the meal
 
 	@Autowired
 	private EntityManager entityManager;
@@ -31,31 +26,25 @@ public class RecipeService {
 
 	// Βασικό URI του API
 	private final String baseUri = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-	private String fullUri;
 
 	public RecipeService(RestTemplate theRestTemplate) {
 		restTemplate = theRestTemplate;
 	}
 
 	//	public ResponseEntity<MealResponse> getRecipesByName(@RequestParam String name) {
-	public Object getRecipesByName(@RequestParam String name) {
-//				Header value is set to (if that exist)
-//				HttpHeaders headers =new HttpHeaders();
-//				headers.set("X-RapidAPi-Key", "XRapidApiKey");
-//				headers.set("null", "null");
-		try {
-// Δημιουργία πλήρους URI
-			fullUri = baseUri + name;
+	public MealResponse getRecipesByName(@RequestParam String name) {
 
-			// Κλήση του εξωτερικού API
-			Object jsonResponse = restTemplate.getForObject(fullUri, Object.class);
-//			******* Επιλέγω ποια στοιχεία θα φέρω μέσα από την κλλάση MealResponse
+		String fullUri = baseUri + name;
+
+		try {
+			// Κλήση του εξωτερικού API και αποθήκευση των δεδομένων σε μεταβλητή
+			ResponseEntity<MealResponse>  jsonResponse = restTemplate.getForEntity(fullUri, MealResponse.class);
+//			******* Επιλέγω ποια στοιχεία θα φέρω μέσα από την κλάση MealResponse
 //			ResponseEntity<MealResponse> jsonResponse = restTemplate.getForEntity(fullUri, MealResponse.class);
 //			********
-			log.info("Output for a API: {}", jsonResponse);
-
+			log.info("Output for a API: {}", jsonResponse.getBody());
 			// Επιστροφή των αποτελεσμάτων
-			return Collections.singletonList(jsonResponse);
+			return jsonResponse.getBody();
 		} catch (Exception e) {
 			log.error(" - Something goes wrong while getting value from API -",e);
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
@@ -63,7 +52,7 @@ public class RecipeService {
 		}
 	}
 
-public CategoryResponse getRecipesByCategory() {
+public CategoryResponse getCategory() {
 		try {
 // Δημιουργία URI categories
 			String catBaseUri = "https://www.themealdb.com/api/json/v1/1/categories.php";
@@ -75,6 +64,26 @@ public CategoryResponse getRecipesByCategory() {
 
 			// Επιστροφή των αποτελεσμάτων
 			return jsonResponse.getBody();
+		} catch (Exception e) {
+			log.error(" - Something goes wrong while getting value from API -",e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Exception while calling endpoint of API", e);
+		}
+	}
+
+	public MealResponse getRecipesByCategory(@RequestParam String name) {
+		try {
+// Δημιουργία URI categories
+			String searchBaseUri = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+			String searchFullUri = searchBaseUri + name;
+
+			// Κλήση του εξωτερικού API
+			MealResponse jsonResponse = restTemplate.getForObject(searchBaseUri, MealResponse.class);
+//			******* Θα Επιλέξω ποια στοιχεία θα φέρω μέσα από την κλάση MealResponse
+			log.info("Output for API: {}", jsonResponse);
+
+			// Επιστροφή των αποτελεσμάτων
+			return jsonResponse;
 		} catch (Exception e) {
 			log.error(" - Something goes wrong while getting value from API -",e);
 			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
