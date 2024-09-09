de3a29a354d1cf8a71114ea9134f01acfd0ca93e
gergefis <votanonkipos@gmail.com>
2024-09-09 21:45:58 +0300

    create Object for external Json data

---
+++ src/main/java/com/mysite/demo/app/recipeat/dto/MealResponse.java
@@ -0,0 +1,54 @@
+package com.mysite.demo.app.recipeat.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+
+//BEGIN - Update class with Lombok to decrease boilerplate
+//...
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MealResponse {
+
+	private List<Meal> meals;
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Getter
+	@Setter
+	public static class Meal {
+
+		private String idMeal;
+
+		private String strMeal;
+		private String strDrinkAlternate;
+		private String strCategory;
+		private String strArea;
+		private String strInstructions;
+		private String strMealThumb;
+		private String strYoutube;
+		private String strIngredient1;
+		private String strIngredient2;
+		private String strIngredient3;
+		private String strIngredient4;
+		private String strIngredient5;
+		private String strIngredient6;
+		private String strIngredient7;
+		private String strIngredient8;
+		private String strMeasure1;
+		private String strMeasure2;
+		private String strMeasure3;
+		private String strMeasure4;
+		private String strMeasure5;
+		private String strMeasure6;
+		private String strMeasure7;
+		private String strMeasure8;
+		// Όλα τα άλλα πεδία μέχρι strIngredient20 και strMeasure20
+	}
+}
