9f141e662e347426cd41631b0aeac7a5b9f150ac
gergefis <votanonkipos@gmail.com>
2024-09-09 21:54:35 +0300

    Set up an Entity Manager for adding recipes

--- src/main/java/com/mysite/demo/app/recipeat/dao/RecipeDAOJpaImpl.java
+++ src/main/java/com/mysite/demo/app/recipeat/dao/RecipeDAOJpaImpl.java
@@ -2,17 +2,12 @@ package com.mysite.demo.app.recipeat.dao;
 
 import com.mysite.demo.app.recipeat.entity.Recipe;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
-public class RecipeDAOJpaImpl implements RecipeDao {
-	// define field for entityManager
+public class RecipeDAOJpaImpl implements RecipeDAO {
+
 	private final EntityManager entityManager;
 
 	@Autowired
@@ -21,26 +16,7 @@ public class RecipeDAOJpaImpl implements RecipeDao {
 	}
 
 	@Override
-	public void save(Recipe theRecipe) {
-		if (theRecipe.getIdMeal() == null || theRecipe.getIdMeal().isEmpty()) {
-			// Insert new recipe
-			entityManager.persist(theRecipe);
-		} else {
-			// Update existing recipe
-			entityManager.merge(theRecipe);
-		}
-	}
-
-	@Override
-	public List<Recipe> findAll() {
-
-		// create a query
-		TypedQuery<Recipe> theQuery = entityManager.createQuery("from Recipe", Recipe.class);
-
-		// execute query and get result list
-		List<Recipe> recipes = theQuery.getResultList();
-
-		// return the results
-		return recipes;
+	public Recipe save(Recipe theRecipe) {
+		return entityManager.merge(theRecipe);
 	}
 }
--- src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
+++ src/main/java/com/mysite/demo/app/recipeat/service/RecipeService.java
@@ -1,14 +1,48 @@
 package com.mysite.demo.app.recipeat.service;
 
-import com.mysite.demo.app.recipeat.entity.Recipe;
-import org.springframework.context.annotation.ComponentScan;
+import com.mysite.demo.app.recipeat.dto.MealResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+@Service
+@Slf4j
+public class RecipeService {
 
-public interface RecipeService {
-	void save(Recipe theRecipe);
+	private final RestTemplate restTemplate;
 
-	List<Recipe> findAll();
+	public RecipeService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
+	public MealResponse jsonResponse(String fullUri){
+		return restTemplate.getForObject(fullUri, MealResponse.class);
+	}
+
+	/**
+	 * Searches for a recipe based on the user's query.
+	 *
+	 * @param mealName The search query provided by the user.
+	 * @return The recipe that matches the query, or null if no match is found.
+	 */
+	public MealResponse getRecipesByName(@RequestParam String mealName) {
+
+		String baseUri = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+		String fullUri = baseUri + mealName;
+
+		try {
+			MealResponse jsonResponse = jsonResponse(fullUri);
+
+			log.error(String.valueOf(jsonResponse));
+
+			return jsonResponse;
+		} catch (Exception e) {
+			log.error(" - Something goes wrong while getting value from API 1 -",e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+					"Exception while calling endpoint of API", e);
+		}
+	}
 }
