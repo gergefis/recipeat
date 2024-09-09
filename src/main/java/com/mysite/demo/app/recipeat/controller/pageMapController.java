1e0d4e14daf6565f4f3b6d7bfd0c8ef62ec5fc26
gergefis <votanonkipos@gmail.com>
2024-09-09 22:00:21 +0300

    To handle the pages views-model

---
+++ src/main/java/com/mysite/demo/app/recipeat/controller/pageMapController.java
@@ -0,0 +1,32 @@
+package com.mysite.demo.app.recipeat.controller;
+
+import com.mysite.demo.app.recipeat.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pageMapController {
+	public final CategoryService categoryService;
+
+	@Autowired
+	public pageMapController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+
+	//	Return the categories names into drop-down list at Index Page
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("categories", categoryService.getCategoryList());
+		return "index";
+	}
+
+	//	Return the categories names into drop-down list to Category Page
+	@GetMapping("/categories")
+	public String categories(Model model) {
+		model.addAttribute("categories", categoryService.getCategoryList());
+		return "categories";
+	}
+
+}
