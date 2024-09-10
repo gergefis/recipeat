1e3c4d0e23915cfb235b9e4041e210f67fddf597
gergefis <votanonkipos@gmail.com>
2024-09-10 19:34:31 +0300

    User entity

--- src/main/java/com/mysite/demo/app/recipeat/user/entity/User.java
+++ src/main/java/com/mysite/demo/app/recipeat/user/entity/User.java
@@ -1,6 +1,5 @@
-package com.mysite.demo.app.recipeat.entity;
+package com.mysite.demo.app.recipeat.user.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
