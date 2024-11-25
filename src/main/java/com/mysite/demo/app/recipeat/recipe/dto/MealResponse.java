8883163d826054be89daf9eed428e423736a8975
gergefis <votanonkipos@gmail.com>
2024-11-25 13:52:56 +0200

    Create Object for external Json data

--- src/main/java/com/mysite/demo/app/recipeat/recipe/dto/MealResponse.java
+++ src/main/java/com/mysite/demo/app/recipeat/recipe/dto/MealResponse.java
@@ -1,7 +1,10 @@
 package com.mysite.demo.app.recipeat.recipe.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
