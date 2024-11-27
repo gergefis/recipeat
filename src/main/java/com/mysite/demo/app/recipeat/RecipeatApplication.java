919ca6d61da3b5eebc7d7e2dcc3ce81f475e048a
gergefis <votanonkipos@gmail.com>
2024-11-27 21:02:41 +0200

    code review and cleanup

--- src/main/java/com/mysite/demo/app/recipeat/RecipeatApplication.java
+++ src/main/java/com/mysite/demo/app/recipeat/RecipeatApplication.java
@@ -1,11 +1,7 @@
 package com.mysite.demo.app.recipeat;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RecipeatApplication {
@@ -13,7 +9,4 @@ public class RecipeatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeatApplication.class, args);
 	}
-
-
-
 }
