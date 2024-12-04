0e8f0b7600fadc340bcef4b2c3d58f1e0494935b
gergefis <votanonkipos@gmail.com>
2024-12-04 11:48:39 +0200

    creating DAO interface to handle recipe objects and entities

--- src/main/java/com/mysite/demo/app/recipeat/recipe/service/RecipeDBService.java
+++ src/main/java/com/mysite/demo/app/recipeat/recipe/service/RecipeDBService.java
@@ -2,8 +2,8 @@ package com.mysite.demo.app.recipeat.recipe.service;
 
 import com.mysite.demo.app.recipeat.recipe.dto.MealResponse;
 import com.mysite.demo.app.recipeat.recipe.entity.Recipe;
+import org.springframework.stereotype.Service;
 
-//@Service
 public interface RecipeDBService {
 	Recipe save(MealResponse.Meal theMeal);
 }
